5e6474cac311262d5fd152a3ffde871bf9f394e5
Ismail <nakkazulav@mail.ru>
2025-03-22 16:25:25 +0400

    remove play/stop

--- popup.js
+++ popup.js
@@ -2,10 +2,6 @@
     document.getElementById("skipAd").addEventListener("click", () => {
         sendMessageToActiveTab({ action: "skipAd" });
     });
-
-    document.getElementById("playPause").addEventListener("click", () => {
-        sendMessageToActiveTab({ action: "playPause" });
-    });
 });
 
 function sendMessageToActiveTab(msg) {
