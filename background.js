31dd7ff850e6f6dc730c4b06537174900ab36d7a
Ismail <nakkazulav@mail.ru>
2025-03-22 15:34:03 +0400

    fix err in background.js

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 ﻿setInterval(() => {
     chrome.tabs.query({ url: "*://*.youtube.com/*" }, tabs => {
         for (let tab of tabs) {
-            chrome.tabs.sendMessage(tab.id, { action: 'skipAd' });
+            chrome.tabs.sendMessage(tab.id, { action: 'skipAd' }, response => {
+                if (chrome.runtime.lastError) {
+                    console.warn("Message failed:", chrome.runtime.lastError.message);
+                }
+            });
         }
     });
 }, 1000);
