112717aa696bd538d09df1facf6b79332bcbe0b7
Ismail <nakkazulav@mail.ru>
2025-03-22 13:32:02 +0400

    initial

---
+++ background.js
@@ -0,0 +1,7 @@
+﻿setInterval(() => {
+    chrome.tabs.query({ url: "*://*.youtube.com/*" }, tabs => {
+        for (let tab of tabs) {
+            chrome.tabs.sendMessage(tab.id, { action: 'skipAd' });
+        }
+    });
+}, 1000);
---
+++ content.js
@@ -0,0 +1,15 @@
+﻿function trySkipAd() {
+    const skipButton = document.querySelector('.ytp-skip-ad-button');
+    const video = document.querySelector('video');
+    if (skipButton && !skipButton.disabled) {
+        console.log('🎯 Skipping...');
+        skipButton.click();
+        video.playbackRate = 16;
+        video.currentTime = video.duration;
+    }
+}
+
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    if (msg.action === 'skipAd') trySkipAd();
+});
